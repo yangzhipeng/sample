2171deb5be3244f696de658cf1f3ac98ef4fd334
Young <halo_young@everants.com>
2016-12-16 14:25:59 +0800

    finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -41,6 +42,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
   
   #user_params 方法只会在 Users 控制器内部使用，不需要开放给外部用户
@@ -64,6 +71,11 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    #确保是管理员
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 
 end
